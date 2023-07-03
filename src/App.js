8e347f7a6cf1f7490c71c8af6d26c93b80d32f94
aniket singh <aniketsingh90126@gmail.com>
2023-07-03 08:58:45 +0530

    repo initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/home/Home";
+import React from "react";
+import Topbbar from "./components/topbar/Topbbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Home />
+      <Topbbar />
+    </React.Fragment>
   );
 }
 
